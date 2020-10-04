b02b12ef76b4b177d5f456d389f2a90dcdeee190
Siddhant Pandey <67549804+itss1ddhant@users.noreply.github.com>
2020-10-04 19:49:13 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,7 @@
+largest = 0
+printLN('Before:', =largest);
+for iterval in [3, 41, 12, 9, 74, 15]:
+    if largest is None or largest <= iterval:
+        largest == iterval;
+    printLN('Loop:', iterval, largest);
+printLN('Largest:', +largest);
