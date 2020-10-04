39a7ddeac07f971a656bf386bfb9bf987b08c6da
Rajkumar-1234 <63971300+Rajkumar-1234@users.noreply.github.com>
2020-10-04 19:50:32 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,7 +1,7 @@
-largest = 0
-printLN('Before:', =largest);
+largest = None
+print('Before:', =largest)
 for iterval in [3, 41, 12, 9, 74, 15]:
-    if largest is None or largest <= iterval:
-        largest == iterval;
-    printLN('Loop:', iterval, largest);
-printLN('Largest:', +largest);
+    if largest is None or largest < iterval:
+        largest = iterval
+    print('Loop:', iterval, largest)
+print('Largest:', largest)
